5375ff24ba79ef5b1ca0a5dabb658c57519ca7e9
balazs.kiss <balazs.kiss@ps.intland.com>
2019-11-17 14:33:29 +0100

    Widget with user tracker items

--- src/main/java/com/intland/codebeamer/dashboard/component/widgets/bootstrap/HelloWorldWidgetHtmlRenderer.java
+++ src/main/java/com/intland/codebeamer/dashboard/component/widgets/bootstrap/HelloWorldWidgetHtmlRenderer.java
@@ -1,9 +1,14 @@
 package com.intland.codebeamer.dashboard.component.widgets.bootstrap;
 
+import com.intland.codebeamer.controller.QueriesResult;
 import com.intland.codebeamer.dashboard.component.common.RenderingContext;
 import com.intland.codebeamer.dashboard.component.common.interfaces.Renderer;
 import com.intland.codebeamer.dashboard.component.widgets.common.attribute.ProjectAttribute;
 import com.intland.codebeamer.dashboard.component.widgets.common.attribute.TrackerAttribute;
+import com.intland.codebeamer.manager.CbQLManager;
+import com.intland.codebeamer.persistence.dao.CbQLPaginatedDtoList;
+import com.intland.codebeamer.persistence.dto.TrackerItemDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +20,9 @@ import java.util.stream.Collectors;
 @Component
 @Qualifier("helloWorldWidgetHtmlRenderer")
 public class HelloWorldWidgetHtmlRenderer implements Renderer<HelloWorldWidget> {
+
+	@Autowired
+	CbQLManager cbQLManager;
 
 	public String render(final RenderingContext renderingContext, final HelloWorldWidget helloWidget) {
 		ProjectAttribute projectAttribute = (ProjectAttribute) helloWidget.getAttributes().get(HelloWorldWidget.Attributes.PROJECT.getKey());
@@ -31,6 +39,14 @@ public class HelloWorldWidgetHtmlRenderer implements Renderer<HelloWorldWidget> 
 				.map(String::valueOf)
 				.collect(Collectors.joining(", "));
 
-		return MessageFormat.format("<strong>Projects and trackers</strong> </br> Selected project ids: {0} </br> Selected tracker ids: {1}", projectIdsString, trackerIdsString);
+		QueriesResult trackerItemQueryResult = cbQLManager.runQuery(renderingContext.getRequest(), MessageFormat.format("project.id IN ({0}) AND tracker.id IN ({1})", projectIdsString, trackerIdsString), new CbQLManager.PagingParams(5, 0));
+		CbQLPaginatedDtoList<TrackerItemDto> trackerItemsPage = trackerItemQueryResult.getTrackerItems();
+		List<TrackerItemDto> trackerItems = trackerItemsPage.getList();
+
+		String trackerItemsRender = trackerItems.stream()
+				.map(trackerItemDto -> MessageFormat.format("{0}({1}): {2}", trackerItemDto.getName(), trackerItemDto.getId().toString(), trackerItemDto.getStatus().getName()))
+				.collect(Collectors.joining("<br>"));
+
+		return MessageFormat.format("<strong>First page of items </strong> </br> {0}", trackerItemsRender);
 	}
 }
